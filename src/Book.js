b4b63839e16bb433336c3beb7f37d15884c827da
Joshua Mun <email": null>
2017-04-25 15:56:02 -0700

    Rendering hello div

---
+++ src/Book.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookItem extends Component {
+  constructor() {
+    super();
+    books = [];
+  }
+  render() {
+    return (
+      <div>Hello!</div>
+    )
+  }
+}
+
+export default BookItem;
