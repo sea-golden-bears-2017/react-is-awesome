fc0b74c54bf9622c6d9424759f133c737c1481cf
Joshua Mun <email": null>
2017-04-25 16:15:57 -0700

    Add book show item title

--- src/Book.js
+++ src/Book.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class BookItem extends Component {
   constructor() {
     super();
-    books = [];
+    this.state = {
+      books: [{ id: 6, title: "The Mermaids Singing", author: "Miss Lilyan McCullough", publisher: "SAGE Publications", genre: "Reference book" }]
+    };
   }
   render() {
     return (
-      <div>Hello!</div>
+      <div className="Header"><h1>{this.state.books[0].title}</h1></div>
     )
   }
 }
