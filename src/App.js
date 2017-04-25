b91bcafb1df77084a2d4cc1ef124d45b814082ef
Joshua Mun <email": null>
2017-04-25 16:36:54 -0700

    Want to show button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-
-import BookItem from './Book'
+import BookItem from './BookItem';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div>
-      <BookItem book={{ id: 6, title: "The Mermaids Singing", author: "Miss Lilyan McCullough", publisher: "SAGE Publications", genre: "Reference book" }} />
+        <BookItem book={{ id: 6, title: "The Mermaids Singing", author: "Miss Lilyan McCullough", publisher: "SAGE Publications", genre: "Reference book" }} />
       </div>
     );
   }
