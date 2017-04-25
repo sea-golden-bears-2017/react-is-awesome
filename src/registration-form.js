e51ca43984e00181b5f2ccd8d9dc09df84be688f
Linsey R.  & Matthew Fischer <pair+linseyo.mattgfisch@devbootcamp.com>
2017-04-25 15:34:07 -0700

    Create registration form and handle input changes

--- src/registration-form.js
+++ src/registration-form.js
@@ -1,10 +1,53 @@
 import React , { Component } from 'react';
 import './App.css';
+import $ from 'jquery';
 
 class RegistrationForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: null,
+      password: null,
+    };
+  }
+
+  handleNameChange(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    });
+  }
+
+  sendRegistration(e) {
+    e.preventDefault();
+    console.log("Registered!");
+    fetch('URL', {
+      method: 'POST',
+      data: 'DATA'
+    }).then(function(response) {
+      console.log(response);
+    })
+    // $.ajax({
+    //   url: "BLANK",
+    //   method: 'POST',
+    //   data:
+    // })
+  }
+
   render() {
     return (
-      <div>Da Reggie form</div>
+      <div>
+        <form className="reggieform" onSubmit={(e) => this.sendRegistration(e)}>
+          <input type="text" name="name" placeholder="name here" onChange={this.handleNameChange.bind(this)} />
+          <input type="password" name="password" placeholder="password here" onChange={this.handlePasswordChange.bind(this)} />
+          <input type="submit" />
+        </form>
+      </div>
     )
   }
 }
