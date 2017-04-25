3010e7a6ad2740851ad1e1ef2a0bdb2a6795c4ea
Dan McKeon & Nick Jarin <pair+danmckeon.njarin@devbootcamp.com>
2017-04-25 12:35:25 -0700

    Add title, login form, attempt to integrate bootstrap - in progress

--- src/App.js
+++ src/App.js
@@ -2,18 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var BS                 = require('react-bootstrap'),
+    Input              = BS.Input,
+    Button             = BS.Button;
+
+class Login extends Component {
+  render() {
+    <form>
+      <Input type='name'
+            name='name'
+            label='Name'
+            placeholder='Enter name...'
+            // disabled={this.props.signedIn}
+            // value={this.state.email}
+            // onChange={this.handleInputChange}
+          />
+
+      <Input type='password'
+            name='password'
+            label='Password'
+            placeholder='Enter password...'
+            // disabled={this.props.signedIn}
+            // value={this.state.password}
+            // onChange={this.handleInputChange}
+          />
+
+      <Button className='btn btn-primary'
+              // onClick={this.handleSignInClick}
+              // disabled={this.props.signedIn}
+              >
+        Sign In
+      </Button>
+    </form>
+  }
+}
+
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Nerdmeter</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content">
+          <Login />
+        </div>
       </div>
     );
   }
