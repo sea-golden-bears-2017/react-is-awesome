58dfaf3bf69b14fb37c66b5eb7d71243ea88b29e
Joshua Mun <email": null>
2017-04-25 15:44:21 -0700

    Add book component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends Component {
   render() {
     return (
+      <div>
       <NavBar />
       <BookContainer />
+      <BookShow />
+      </div>
     );
   }
 }
