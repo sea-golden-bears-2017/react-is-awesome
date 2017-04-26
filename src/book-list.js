162a4a9752b817e08de01814b3956125b581d9bf
Linsey R.  & Matthew Fischer <pair+linseyo.mattgfisch@devbootcamp.com>
2017-04-26 15:01:46 -0700

    Commit before experimenting with mounting Book

--- src/book-list.js
+++ src/book-list.js
@@ -4,21 +4,38 @@ import Book from './book'
 import $ from 'jquery'
 
 class BookList extends Component {
+  constructor(){
+    super();
+    this.state = {
+      allBooks: null,
+    }
+  }
   getBooks() {
+    let bookList = null;
     $.ajax({
       url: 'http://localhost:3000/books/',
       method: 'GET',
       crossDomain: true,
       xhrFields: { withCredentials: true }
     }).done((response) => {
-      
+      return bookList = (
+        response.map((book) => {
+          return (
+            <Book bookInfo={book} />
+          )
+        })
+      );
     });
+    debugger;
   }
+  // renderBooks(){
+  //
+  // }
   render() {
-    this.getBooks()
+
     return (
       <div>
-        <Book />
+        {this.getBooks()}
       </div>
 
     )
--- src/book.js
+++ src/book.js
@@ -2,9 +2,12 @@ import React , { Component } from 'react';
 import './App.css';
 
 class Book extends Component {
+
   render() {
     return (
-      <div>BOOK
+
+      <div>
+        {this.props.bookInfo.title}
       </div>
 
     )
