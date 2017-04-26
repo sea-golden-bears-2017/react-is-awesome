a45ce01958c803a9a329f4d9748cc1718010c5d4
Eitan Greene & Julia(e) Xu <pair+eitan-g.jmxu94@devbootcamp.com>
2017-04-25 17:31:34 -0700

    Creates click handler for delete button

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
 class Book extends Component {
+  handleClick() {
+    console.log("ALERT: HI!")
+  }
   render() {
     return (
       <div>
         <button className='bookLink'>
           {`${this.props.book.title} by ${this.props.book.author}`}
         </button>
-        <button className='delete_btn'>
+        <button onClick={this.handleClick} className='delete_btn'>
           Delete Book
         </button>
       </div>
