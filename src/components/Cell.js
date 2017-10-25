f22e948e16017fcc757cc354eaa094af974dfe63
Miguel Angel Ibarra, Jr. & Eliza Brine <pair+Migaibarra.ebrine@devbootcamp.com>
2017-10-25 14:46:25 -0700

    Turn bingo squares color if user has read a book of that genre

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -10,9 +10,19 @@ class Cell extends Component {
 
 
 
+
   render() {
+    const getRandomColor = () => {
+      const colors = ["aquamarine", 'olive', 'bisque', 'coral', 'darkslategray', 'indianred']
+      return colors[Math.floor(Math.random() * 6)]
+    }
+    const setColorStyle = () => {
+      if(this.props.hasReadABook === 'true') {
+      return  {'background-color': getRandomColor()}
+      }
+    }
     return (
-      <div className="Cell" id={this.props.genre} onClick={this.props.onClick}>
+      <div className={"Cell " + this.props.hasReadABook} id={this.props.genre} onClick={this.props.onClick} style={setColorStyle()}>
         <p>{this.props.genre}</p>
         <p>{this.props.hasReadABook}</p>
       </div>
